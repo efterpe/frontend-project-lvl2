02e23101d4ffe14df1bf185490b3b2272ed5a13f
Natalia Markelova <natalia@markelova.online>
2021-06-05 20:58:47 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,8 @@
 import program from 'commander';
 
 program.version('0.1.0');
+program.arguments('<filepath1> <filepath2>');
+program.option('-f, --format [type]', ' output format');
 program.description('Compares two configuration files and shows a difference.');
 
 program.parse();
