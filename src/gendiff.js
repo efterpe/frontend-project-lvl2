27561d41406edb2e5e6fbbc35d4107d3b9e3d2a4
Natalia Markelova <natalia@markelova.online>
2021-06-29 11:15:19 +0300

    Revert "step3 gendiff"
    This reverts commit dda75eac4dcb07a147c9255321485ff521aa4134.
    
    :qw
    
    :q

--- src/gendiff.js
+++ src/gendiff.js
@@ -8,30 +8,12 @@ const getData = (filepath) => {
   const fullpath = path.resolve(process.cwd(), filepath);
   return fs.readFileSync(fullpath, 'utf-8');
 };
-const parseFile = (file) => {
-  const result = JSON.parse(file);
-  return result;
-};
 
 const getDiff = (filepath1, filepath2) => {
-  const file1 = parseFile(getData(filepath1));
-  const file2 = parseFile(getData(filepath2));
-  const result = {};
-
-  for (const key in file1) {
-    if (_.has(file2, key)) {
-      if (file1[key] === file2[key]) {
-        result.push(file1[key]);
-      } else {
-        result.push(`-${file1[key]}`);
-        result.push(`+${file2[key]}`);
-      }
-    }
-
-    console.log(file1, file2);
-  }
+  const file1 = getData(filepath1);
+  const file2 = getData(filepath2);
+  console.log(file1, file2);
 };
-
 export default getDiff;
 
 // console.log(fs.readFileSync(path.resolve(process.cwd(), '../file1.json'), 'utf-8'));
